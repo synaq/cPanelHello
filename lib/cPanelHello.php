6a9992ce887f0bd5689b28a9d0262cea8f4079bd
willemv <willemv@synaq.com>
2017-06-28 13:14:33 +0200

    Fixed incorrect autoloader usage

--- lib/cPanelHello.php
+++ lib/cPanelHello.php
@@ -1,5 +1,5 @@
 <?php
 $apiConnector = '';
-require_once('../vendor/composer/autoload_real.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 $container = \Synaq\HelloWorld\Factory\DependencyInjectionContainerFactory::create();
 $apiConnector = $container->create(\Synaq\HelloWorld\ApiConnector\PublicConfigurationApiConnectorInterface::class);
