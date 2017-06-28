1e67cf72523ea7dade74caa8076488705c56e6e0
willemv <willemv@synaq.com>
2017-06-28 10:12:40 +0200

    DependencyInjectionContainerFactoryCreate configuresContainerToReturnHttpPublicConfigurationApiConnectorInstanceWhenAskedForInstanceOfPublicConfigurationApiConnectorInterface

--- src/Synaq/HelloWorld/Factory/DependencyInjectionContainerFactory.php
+++ src/Synaq/HelloWorld/Factory/DependencyInjectionContainerFactory.php
@@ -10,11 +10,18 @@ namespace Synaq\HelloWorld\Factory;
 
 
 use Dice\Dice;
+use Synaq\HelloWorld\ApiConnector\HttpPublicConfigurationApiConnector;
+use Synaq\HelloWorld\ApiConnector\PublicConfigurationApiConnectorInterface;
 
 class DependencyInjectionContainerFactory
 {
     public function create()
     {
-        return new Dice();
+        $container = new Dice();
+        $container->addRule(PublicConfigurationApiConnectorInterface::class, [
+            'instanceOf' => HttpPublicConfigurationApiConnector::class
+        ]);
+
+        return $container;
     }
 }
--- src/Tests/Synaq/HelloWorld/Factory/DependencyInjectionContainerFactoryCreateTest.php
+++ src/Tests/Synaq/HelloWorld/Factory/DependencyInjectionContainerFactoryCreateTest.php
@@ -9,17 +9,42 @@
 namespace Tests\Synaq\HelloWorld\Factory;
 
 use Dice\Dice;
+use Synaq\HelloWorld\ApiConnector\HttpPublicConfigurationApiConnector;
+use Synaq\HelloWorld\ApiConnector\PublicConfigurationApiConnectorInterface;
 use Synaq\HelloWorld\Factory\DependencyInjectionContainerFactory;
 
 class DependencyInjectionContainerFactoryCreateTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var Dice
+     */
+    private $container;
+
+    /**
+     * @var DependencyInjectionContainerFactory
+     */
+    private $factory;
+
+    /**
      * @test
      */
     public function returnsDiceDependencyInjectionContainer()
     {
-        $factory = new DependencyInjectionContainerFactory();
-        $container = $factory->create();
-        $this->assertInstanceOf(Dice::class, $container);
+        $this->assertInstanceOf(Dice::class, $this->container);
+    }
+
+    /**
+     * @test
+     */
+    public function configuresContainerToReturnHttpPublicConfigurationApiConnectorInstanceWhenAskedForInstanceOfPublicConfigurationApiConnectorInterface(
+    ) {
+        $connector = $this->container->create(PublicConfigurationApiConnectorInterface::class);
+        $this->assertInstanceOf(HttpPublicConfigurationApiConnector::class, $connector);
+    }
+
+    protected function setUp()
+    {
+        $this->factory = new DependencyInjectionContainerFactory();
+        $this->container = $this->factory->create();
     }
 }
